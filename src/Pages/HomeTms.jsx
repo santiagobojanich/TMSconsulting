6abd3f8a5c44131320b424fdf926cca918cce894
santiagobojanich <santidgz@hotmail.com>
2023-05-04 18:08:03 -0300

    About1 y 2

--- src/Pages/HomeTms.jsx
+++ src/Pages/HomeTms.jsx
@@ -1,16 +1,34 @@
 import Home from '../components/Home';
 import NavBar from '../components/NavBar';
+import About2 from './About2';
+import AboutUs from './AboutUs';
+
 
  const HomeTms = () => {
     
+
     return (
-        <div className='h-screen bg-bgBlack'>
+      <div>
+
     
-        <div className='h-auto border-white'>
-         <NavBar />
-        </div>
-  
-         <Home/>
+          <div className='h-screen bg-bgBlack'>
+
+           <div className='h-auto border-white'>
+             <NavBar />
+           </div>
+             <Home/>
+          </div>
+      
+          <div className='h-screen'>
+            <AboutUs/>
+          </div>
+
+          
+          <div className='h-screen'>
+            <About2/>
+          </div>
+          
+
       </div>
     )
 
